dec180ebefe76a33ca750d73c2ea17353eef65e7
Neman Haciyev <zaraganq@gmail.com>
2025-01-21 19:03:53 +0400

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './Css/Main.Css'
+import './Css/Main.css'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
