7ce44fadf5a220ebc053f374e5299aec97a187af
Neman Haciyev <zaraganq@gmail.com>
2025-01-22 15:51:28 +0400

    Updated Movie Details Modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import '../Css/Modal.css'
 
 const Modal = ({ closeModal, movie }) => {
+    console.log(movie)
     return (
         <div className='modal-card'>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+            <span onClick={closeModal} className='close'>X</span>
+            <div className='img-poster'>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className='img' alt="" />
+            </div>
+            <strong>Overview</strong>
+            <p>{movie.overview}</p>
+            <p>Original Language : {movie.original_language}</p>
+            <p>Movie Name : {movie.original_title}</p>
+            <p>Raiting : {movie.vote_count}</p>
         </div>
     )
 }
